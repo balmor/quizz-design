850c8c8f25a415e460f686b2ccb443268047cab1
karolgorecki <kgorecki@nexway.com>
2013-12-13 11:30:25 +0100

    Intro page updated 	modified:   intro.php 	new file:   less/intro.less 	modified:   less/main.less 	new file:   nyancat.ogg

--- intro.php
+++ intro.php
@@ -32,92 +32,26 @@
         <div class="top-navbar">
             <a href="#" title="Full Screen on/off" id="fullscreen-button"><div class="monitor"><span class="hide">Full Screen on/off</span></div></a>
         </div>
-        <section>
+        <section class="intro">
 
-            <div class="question-container">
-                <div class="question">
-                    <span class="text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, id, ea, et deserunt numquam atque ullam rem explicabo sint quibusdam quasi libero tempora qui? Quaerat id odit earum nisi consectetur. Ipsam nesciunt laudantium blanditiis inventore! Dolorum, odit, tempore, sint, placeat porro fugit expedita voluptatibus tenetur deserunt nihil quae corrupti libero error impedit a quam officiis provident voluptatum inventore autem quos sunt repellat! Sequi, explicabo, corporis.
-                    </span>
-                </div>
-            </div>
-
-            <div class="timer-container">
-                <div class="time-left"></div>
-            </div>
-
-            <div class="spacer-v-2"></div>
-
-            <div class="content">
-                <div class="wrapper-answers">
-                    <ul class="answers">
-                        <li class="blue incorrect">ANSWER</li>
-                        <li class="orange incorrect">ANSWER</li>
-                        <li class="green correct">ANSWER</li>
-                        <li class="yellow incorrect">ANSWER</li>
-                    </ul>
-                </div>
-                <div class="wrapper-players">
-                    <ul class="players">
-                        <li class="player">
-                            <div class="status">
-                                <div class="checked"><span></span></div>
-                            </div>
-                            <div class="info-player">
-                                <div class="name">balmor</div>
-                                <div class="points">11000</div>
-                            </div>
-                            <div class="avatar">
-                                <div class="image"><img src="http://www.gravatar.com/avatar/<?php echo md5('dduda@nexway.com') ?>?s=100" alt=""></div>
-                            </div>
-                        </li>
-                        <li class="player">
-                            <div class="status selected green correct">
-                                <div class="checked"><span></span></div>
-                            </div>
-                            <div class="info-player">
-                                <div class="name">mariusz</div>
-                                <div class="points">9200</div>
-                            </div>
-                            <div class="avatar">
-                                <div class="image"><img src="http://www.gravatar.com/avatar/<?php echo md5('maronmariusz@gmail.com') ?>?s=100" alt=""></div>
-                            </div>                                
-                        </li>
-                        <li class="player">
-                            <div class="status selected blue incorrect">
-                                <div class="checked"><span></span></div>
-                            </div>
-                            <div class="info-player">
-                                <div class="name">karol</div>
-                                <div class="points">7000</div>
-                            </div>
-                            <div class="avatar">
-                                <div class="image"><img src="http://www.gravatar.com/avatar/<?php echo md5('kgorecki@nexway.com') ?>?s=100" alt=""></div>
-                            </div>                                
-                        </li>
-                        <li class="player">
-                            <div class="status selected">
-                                <div class="checked"><span></span></div>
-                            </div>
-                            <div class="info-player">
-                                <div class="name">michal</div>
-                                <div class="points">6300</div>
-                            </div>
-                            <div class="avatar">
-                                <div class="image"><img src="http://www.gravatar.com/avatar/<?php echo md5('chajr@bluetree.pl') ?>?s=100" alt=""></div>
-                            </div>                                
-                        </li>                 
-                    </ul>
-                </div>
-            </div>
+            
 
         </section>
 
     </div>
 
+    <audio autoplay="true" src="https://dl.dropboxusercontent.com/u/41442178/nyancat.ogg" loop="loop">
+        <script>
+            document.getElementsByTagName('audio')[0].addEventListener('ended', function(){
+                this.currentTime = 0;
+            }, false);
+    </script>
+
+    </audio>
     <script src="//code.jquery.com/jquery.min.js" type="text/javascript"></script>
     <script src="//code.jquery.com/ui/1.9.0/jquery-ui.js" type="text/javascript"></script>
     <script src="js/custom.js" type="text/javascript"></script>
+
 
 </body>
 </html>
